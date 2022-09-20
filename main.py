94d04147a23f94007793a45841b74fac8b00f807
Ruberoid <refafa@yandex.ru>
2022-09-20 23:15:48 +0300

    added get_shop_list_by_dishes()

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
 # Задание: https://github.com/netology-code/py-homeworks-basic/tree/master/7.files
 
-def filetodict(file, sep='|'):
+def filetodict(file, sep=' | '):
     tmplist = list()
     cook_book = {}
-    tmpdict = {}
     tl3 = list()
     f = open(file, mode='r', encoding='utf-8')
-
     for line in f:
         if line != '\n':
             tmplist.append(line.replace('\n', ''))
         else:
             for i in range(2, len(tmplist)):
                 tl2 = tmplist[i].split(sep)
-                tl3.append({'ingredient_name': tl2[0], 'quantity': tl2[1], 'measure': tl2[2]})
+                tl3.append({'ingredient_name': tl2[0], 'quantity': int(tl2[1]), 'measure': tl2[2]})
             cook_book.update({tmplist[0]: tl3})
             tmplist = list()
             tl3 = list()
-    for i in cook_book:
-        print(i)
-        for j in cook_book[i]:
-            print(f"  {j}")
-        pass
-    # print(cook_book)
+    return cook_book
 
-filetodict('files.txt')
+def get_shop_list_by_dishes(cook_book, dishes, person_count):
+    tmplist = []
+    tmpdict = {}
+    for i in cook_book:
+        if dishes.count(i) > 0:
+            for j in cook_book[i]:
+                for k in tmplist:
+                    if k['ingredient_name'] == j['ingredient_name']:
+                        tmplist[tmplist.index(k)]['quantity'] += j['quantity']
+                        tmplist[tmplist.index(k)]['quantity'] *= person_count
+                        break
+                else:
+                    j['quantity'] *= person_count
+                    tmplist.append(j)
+    for i in tmplist:
+        ti = i['ingredient_name']
+        i.pop('ingredient_name')
+        tmpdict.update({ti: i})
+    return tmpdict
+
+# filetodict('files.txt')
+t = get_shop_list_by_dishes(filetodict('files.txt'), ['Запеченный картофель', 'Омлет'], 3)
+
+for i in t:
+    print(f"{i}: {t[i]}")
