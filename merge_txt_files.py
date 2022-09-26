00a61b8e438606f1c68f6e3919162023888c7853
Ruberoid13 <refafa@yandex.ru>
2022-09-26 17:30:16 +0300

    merge_txt_files - added '\n' check cook_list - works fine

--- merge_txt_files.py
+++ merge_txt_files.py
@@ -1,5 +1,17 @@
-def create_dict():
-    file_name_list = ['1.txt', '2.txt', '3.txt']
+import os
+
+
+def get_txt_files_list():
+    files_list = []
+    for i in os.listdir():
+        if i.count('.txt') > 0 and i != 'result.txt':
+            files_list.append(i)
+    return files_list
+
+
+def create_dict(file_name_list=None):
+    if file_name_list is None:
+        file_name_list = ['1.txt', '2.txt', '3.txt']
     file_dict = {}
     for file in file_name_list:
         with open(file, mode='r', encoding='UTF-8') as f:
@@ -21,7 +33,9 @@ def make_file(in_dict):
     with open('result.txt', mode='w', encoding='UTF-8') as new_file:
         for i in in_dict:
             with open(i, mode='r', encoding='UTF-8') as f:
-                new_file.write(f'{i}\n{in_dict[i]}\n{f.read()}\n')
+                new_file.write(f'{i}\n{in_dict[i]}\n{f.read()}')
+                if i != list(in_dict.keys())[-1::][0]:
+                    new_file.write('\n')
 
 
-make_file(sort_dict(create_dict()))
+make_file(sort_dict(create_dict(get_txt_files_list())))
